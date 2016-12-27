f2b0b2bccb3b22f81dca83c86e26118090519779
Noma <1181098979@qq.com>
2016-12-27 23:08:15 +0800

    冒泡排序手动输入数组
    空格隔开

---
+++ BubbleSort/BubbleSort/2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 2016/9/19
+/// </summary>
+/// 
+namespace BubbleSort
+{
+
+	/// <summary>
+	/// 主程序
+	/// </summary>
+	/// 
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+			string[] inputnum = input.Split(' ');
+			int[] arry = new int[inputnum.Length];
+			for (int i = 0; i < inputnum.Length; i++)
+			{
+				arry[i] = int.Parse(inputnum[i]);
+			}
+
+			Sort.BubbleSort(arry);
+			for (int i = 0; i < arry.Length; i++)
+			{
+				Console.Write("{0} ",arry[i]);
+			}
+			Console.Read();
+		}
+	}
+
+	/// <summary>
+	/// 冒泡排序算法函数
+	/// </summary>
+	/// 
+	static class Sort
+	{
+		public static void BubbleSort(this int[] arry)
+		{
+			for (int i = 0; i < arry.Length; i++)
+				for (int j = 0; j < arry.Length - 1 - i; j++)
+				{
+					if (arry[j] > arry[j + 1])
+					{
+						int temp = arry[j + 1];
+						arry[j + 1] = arry[j];
+						arry[j] = temp;
+					}
+				}
+		}
+	}
+}
